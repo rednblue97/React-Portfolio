af40a40a8a23ae05eb06413071a4b3abf389e680
CaptureB <airdavid62@gmail.com>
2023-05-02 15:37:22 -0600

    Created react app intro component

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Intro from './components/intro';
+
+function App() {
+  return (
+    <div>
+      <Intro></Intro>
+    </div>
+  );
+}
+
+export default App;
